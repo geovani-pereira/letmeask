1140d1fd06f2bca8ba4196b0d88dc8b7fcdf9338
Geovani Pereira <geovanimac@hotmail.com>
2021-06-27 17:59:06 -0300

    Sala de perguntas, inserindo e escutando perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
 
 
 import { AuthContextProvider} from './context/AuthContext'
+import { Room } from './pages/Room';
 
 
 function App() {
@@ -13,8 +14,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home}/>
-        <Route path="/rooms/new" component={NewRoom}/>  
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>    
+        </Switch>
       </AuthContextProvider>    
     </BrowserRouter>
     
